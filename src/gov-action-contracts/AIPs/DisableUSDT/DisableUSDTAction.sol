09259b1ba4b4b35753d1b72cd1f5ff7cb8bfad7e
Henry <11198460+godzillaba@users.noreply.github.com>
2025-02-14 13:22:19 -0600

    format and snapshot

--- src/gov-action-contracts/AIPs/DisableUSDT/DisableUSDTAction.sol
+++ src/gov-action-contracts/AIPs/DisableUSDT/DisableUSDTAction.sol
@@ -15,7 +15,8 @@ interface IL1GatewayRouter {
 /// @notice Set the USDT gateway to address(1) to disable USDT on the token bridge
 contract DisableUSDTAction {
     /// @notice The token bridge L1 gateway router
-    IL1GatewayRouter public constant gatewayRouter = IL1GatewayRouter(0x72Ce9c846789fdB6fC1f34aC4AD25Dd9ef7031ef);
+    IL1GatewayRouter public constant gatewayRouter =
+        IL1GatewayRouter(0x72Ce9c846789fdB6fC1f34aC4AD25Dd9ef7031ef);
     /// @notice The USDT token address
     address public constant usdt = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
     /// @notice The new gateway address (address(1) disables the token)
@@ -32,14 +33,12 @@ contract DisableUSDTAction {
         // send the UpgradeExecutor balance as submission fee
         // the UpgradeExecutor must be funded with selfdestruct before action execution
         gatewayRouter.setGateways{value: address(this).balance}(
-            tokens,
-            gateways,
-            0,
-            0,
-            address(this).balance
+            tokens, gateways, 0, 0, address(this).balance
         );
 
         // check that the gateway was set correctly
-        require(gatewayRouter.getGateway(usdt) == address(0), "DisableUSDTAction: gateway not disabled");
+        require(
+            gatewayRouter.getGateway(usdt) == address(0), "DisableUSDTAction: gateway not disabled"
+        );
     }
 }
--- test/gov-actions/DisableUSDTAction.t.sol
+++ test/gov-actions/DisableUSDTAction.t.sol
@@ -8,52 +8,74 @@ import {IUpgradeExecutor} from "src/interfaces/IUpgradeExecutor.sol";
 import {DisableUSDTAction} from "src/gov-action-contracts/AIPs/DisableUSDT/DisableUSDTAction.sol";
 
 interface IL1GatewayRouter {
-    function outboundTransfer(address _token, address _to, uint256 _amount, uint256 _maxGas, uint256 _gasPriceBid, bytes calldata _data) external payable returns (bytes memory);
+    function outboundTransfer(
+        address _token,
+        address _to,
+        uint256 _amount,
+        uint256 _maxGas,
+        uint256 _gasPriceBid,
+        bytes calldata _data
+    ) external payable returns (bytes memory);
 }
 
 contract DisableUSDTActionTest is Test {
     using SafeERC20 for IERC20;
+
+    uint256 l2ForkBlock = 306_033_004;
+    uint256 l1ForkBlock = 21_845_939;
+
     DisableUSDTAction public disableUSDTAction;
     IUpgradeExecutor ue = IUpgradeExecutor(0x3ffFbAdAF827559da092217e474760E2b2c3CeDd);
     address timelock = 0xE6841D92B0C345144506576eC13ECf5103aC7f49;
-    IL1GatewayRouter public gatewayRouter = IL1GatewayRouter(0x72Ce9c846789fdB6fC1f34aC4AD25Dd9ef7031ef);
+    IL1GatewayRouter public l1GatewayRouter =
+        IL1GatewayRouter(0x72Ce9c846789fdB6fC1f34aC4AD25Dd9ef7031ef);
     IERC20 public usdt = IERC20(0xdAC17F958D2ee523a2206206994597C13D831ec7);
 
-    error InsufficientSubmissionCost(uint,uint);
+    error InsufficientSubmissionCost(uint256, uint256);
 
     function setUp() public {
-        vm.createSelectFork(vm.envString("ETH_URL"), 21833226);
+        vm.createSelectFork(vm.envString("ETH_URL"), l1ForkBlock);
         disableUSDTAction = new DisableUSDTAction();
     }
 
     function testPerform() public {
         // should fail without funding
-        vm.expectRevert(abi.encodeWithSelector(InsufficientSubmissionCost.selector, 2441046598032, 0));
-        _upgrade();
+        vm.expectRevert(
+            abi.encodeWithSelector(InsufficientSubmissionCost.selector, 3_569_349_724_304, 0)
+        );
+        _upgradeL1();
 
         // should pass with funding
-        vm.deal(address(ue), 2441046598032);
-        _upgrade();
+        vm.deal(address(ue), 3_569_349_724_304);
+        _upgradeL1();
     }
 
     function testBridgeDisabled() public {
         address whale = 0xF977814e90dA44bFA03b6295A0616a897441aceC;
         vm.startPrank(whale);
         usdt.safeApprove(0xcEe284F754E854890e311e3280b767F80797180d, type(uint256).max);
-        vm.expectRevert(abi.encodeWithSelector(InsufficientSubmissionCost.selector, 3168812043408, 0));
-        gatewayRouter.outboundTransfer(address(usdt), whale, 10e6, 0, 0, abi.encode(uint256(0), bytes("")));
+        vm.expectRevert(
+            abi.encodeWithSelector(InsufficientSubmissionCost.selector, 4_633_503_679_376, 0)
+        );
+        l1GatewayRouter.outboundTransfer(
+            address(usdt), whale, 10e6, 0, 0, abi.encode(uint256(0), bytes(""))
+        );
         vm.stopPrank();
 
-        vm.deal(address(ue), 2441046598032);
-        _upgrade();
+        vm.deal(address(ue), 3_569_349_724_304);
+        _upgradeL1();
 
         vm.prank(whale);
         vm.expectRevert(bytes(""));
-        gatewayRouter.outboundTransfer(address(usdt), whale, 10e6, 0, 0, abi.encode(uint256(0), bytes("")));
+        l1GatewayRouter.outboundTransfer(
+            address(usdt), whale, 10e6, 0, 0, abi.encode(uint256(0), bytes(""))
+        );
     }
 
-    function _upgrade() internal {
+    function _upgradeL1() internal {
         vm.prank(timelock);
-        ue.execute(address(disableUSDTAction), abi.encodeWithSelector(disableUSDTAction.perform.selector));
+        ue.execute(
+            address(disableUSDTAction), abi.encodeWithSelector(disableUSDTAction.perform.selector)
+        );
     }
 }
